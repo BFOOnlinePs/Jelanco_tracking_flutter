1daf78ffdcd90089ff8f59df8fdc5e82a36b042a
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2024-07-23 13:20:55 +0300

    app name + icon

--- lib/core/values/assets_keys.dart
+++ lib/core/values/assets_keys.dart
@@ -1,8 +1,9 @@
 class AssetsKeys {
   // static const String splashLogo = 'splash_logo';
   static const String appLogo = 'assets/images/jelanco_logo.png';
-
+  static const String appLauncherLogo = 'assets/images/jelanco_vertical.png';
   // static const String appIcon = 'app_icon';
 
   static const String translations = 'assets/translations';
+
 }
